53cccae222e50c1d76cf2936c56052559afad334
Metaleg <vitas-25@mail.ru>
2019-08-21 10:16:03 +0300

    started new course, added info and stepik_2.2.6

---
+++ Algorithms/Algorithms: theory and practice. Methods/stepik_2.2.6.py
@@ -0,0 +1,25 @@
+Задача на программирование: небольшое число Фибоначчи
+
+Дано целое число 1≤n≤40, необходимо вычислить n-е число Фибоначчи (напомним, что F0=0, F1=1 и Fn=Fn−1+Fn−2 при n≥2).
+
+Sample Input:
+3
+
+Sample Output:
+2
+
+
+def fib(n):
+    arr = [0, 1]
+    for i in range(2, n + 1):
+        arr.append(arr[i - 1] + arr[i - 2])
+    return arr[-1]
+
+
+def main():
+    n = int(input())
+    print(fib(n))
+
+
+if __name__ == "__main__":
+    main()
